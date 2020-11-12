c574772b4e3a9620f23bf41e63f93af0bc22338a
Adam8234 <acorpstein8234@gmail.com>
2020-11-12 01:37:20 -0600

    Add denormalize categorical updates and deletions.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -6,16 +6,19 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 const usersCollection = db.collection("users");
 
+/**
+ * Whenever an expense is added, denormalize user data and cateorial data to make it easier on the client
+ */
 exports.addExpense = functions.firestore
   .document("users/{userId}/expense_queue/{docId}")
   .onCreate(async (snap, context) => {
     let data = snap.data();
-    let userRef = usersCollection.doc(context.params.userId);
-    let expensesRef = userRef.collection("expenses");
-    let userCategoriesRef = userRef.collection("categories");
+    const userRef = usersCollection.doc(context.params.userId);
+    const expensesRef = userRef.collection("expenses");
+    const userCategoriesRef = userRef.collection("categories");
 
-    let userSnapshot = await userRef.get();
-    let categorySnapshot = await userCategoriesRef.doc(data.categoryId).get();
+    const userSnapshot = await userRef.get();
+    const categorySnapshot = await userCategoriesRef.doc(data.categoryId).get();
 
     //Denormalize category
     data.category = categorySnapshot.data();
@@ -33,6 +36,15 @@ exports.addExpense = functions.firestore
     snap.ref.delete();
   });
 
+// exports.deleteExpense = functions.firestore
+//   .document("users/{userId}/expense/{docId}")
+//   .onDelete((snap, context) => {
+//     //Do nothing... for now...
+//   });
+
+/**
+ * Whenever a use changes, we need to update our denormalized data
+ */
 exports.writeUser = functions.firestore
   .document("users/{userId}")
   .onWrite(async (change, context) => {
@@ -53,4 +65,41 @@ exports.writeUser = functions.firestore
       batch.update(snapshot.ref, data);
     }
     return batch.commit();
+  });
+
+/**
+ * Update denormalized data for category in expenses
+ */
+exports.updateCategory = functions.firestore
+  .document("users/{userId}/categories/{docId}")
+  .onUpdate(async (change, context) => {
+    const expensesSnapshots = await usersCollection
+      .doc(context.params.userId)
+      .collection("expenses")
+      .where("categoryId", "==", context.params.docId)
+      .get();
+    const batch = db.batch();
+    for (var snapshot of expensesSnapshots.docs) {
+      batch.update(snapshot.ref, { category: change.after.data() });
+    }
+    batch.commit();
+  });
+
+/**
+ * Delete expenses if we delete category
+ */
+exports.deleteCategory = functions.firestore
+  .document("users/{userId}/categories/{docId}")
+  .onDelete(async (snap, context) => {
+    const id = snap.id;
+    const expenses = await usersCollection
+      .doc(context.params.userId)
+      .collection("expenses")
+      .where("categoryId", "==", id)
+      .get();
+    const batch = db.batch();
+    for (var snapshot of expenses.docs) {
+      batch.delete(snapshot.ref);
+    }
+    batch.commit();
   });
